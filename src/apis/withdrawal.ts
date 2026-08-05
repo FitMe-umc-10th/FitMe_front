689548bab3e58831fc00309da901d58caf9c9554
Wook <ohkingsman@naver.com>
2026-08-04 23:28:45 -0700

    fix: 회월탈퇴 api 주소 변경

--- src/apis/withdrawal.ts
+++ src/apis/withdrawal.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from './axiosInstance';
 
 export const requestWithdrawal = async (): Promise<void> => {
-  await axiosInstance.delete('/api/v1/users/me');
+  await axiosInstance.delete('/api/auth');
 };
+
